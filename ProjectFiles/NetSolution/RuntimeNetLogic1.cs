7a6eb0ad372f223a707fbce155601b7b49d408ea
xavierflorensa <xavier.florensa@yahoo.com>
2024-01-12 22:34:50 +0100

    First version. Adding a button to connect and publish "Hello World"

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,55 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using HiveMQtt.Client;
+using HiveMQtt.MQTT5.Types;
+using System.Net.Http;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+        // Setup Client options and instantiate
+        /*var options = new HiveMQClientOptionsBuilder().WithBroker("broker.hivemq.com")
+            .WithPort(1883)
+            .WithUseTls(false)
+            .Build();
+    */
+       //var client = new HiveMQClient(options);
+        // Connect to the MQTT broker
+        //var connectResult = await client.ConnectAsync().ConfigureAwait(false);
+        // Publish a message
+        //var publishResult = await client.PublishAsync("topic1/example", "Hello World");
+        
+        }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+    [ExportMethod]
+    
+    public async void Publish()
+    {
+        // Setup Client options and instantiate
+        var options = new HiveMQClientOptionsBuilder().WithBroker("broker.hivemq.com")
+            .WithPort(1883)
+            .WithUseTls(false)
+            .Build();
+        var client = new HiveMQClient(options);
+        // Connect to the MQTT broker
+        var connectResult = await client.ConnectAsync().ConfigureAwait(false);
+        // Publish a message
+        var publishResult = await client.PublishAsync("topic1/example", "Hello World");
+    }
+}
